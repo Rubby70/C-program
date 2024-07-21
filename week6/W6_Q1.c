5e1df26affd0700fcaa9a1915f136c732e51432b
bear19990220 <bear19990220@gmail.com>
2024-07-21 18:02:48 +0800

    0721-3

--- week6/W6_Q1.c
+++ week6/W6_Q1.c
@@ -1,7 +1,7 @@
 // 寫出strstr函數
 // 當我給你一個字串 str 以及一個目標字串str2 
 // 告訴我目標str2字串是否存在在str當中
-// 如果不存在  則回傳-1。 
+// 如果不存在則回傳-1
 // 存在的話就把他在str當中的位置印出來(0-indexed)
 
 #include <stdio.h>
@@ -10,22 +10,26 @@
 
 
 void string_scan(char **); //讀取輸入的文字
+int count_string(char *); //計算字串總字數
+void ststst(char *, char *, int, int);
 
 int main(void){
 
     printf("\n");
     char *str1; //建立一個叫做str1的字元指標
     printf("請輸入字串1: ");
-    string_scan(&str1); //讀取輸入的字串，並將str1指向該字串
-
+    string_scan(&str1); //讀取輸入的字串
+    int length1 = count_string(str1); //紀錄str1有幾個字
+    
     char *str2;
     printf("請輸入字串2: ");
-    string_scan(&str2); //讀取輸入的字串，並將str2指向該字串
+    string_scan(&str2); //讀取輸入的字串
+    int length2 = count_string(str2); //紀錄str2有幾個字
 
-    printf("\n");
-    printf("str1 = %s\n", str1);
-    printf("str2 = %s\n", str2);
-    printf("\n");
+    printf("str1 = %s ,共有%d個字\n", str1, length1);
+    printf("str2 = %s ,共有%d個字\n", str2, length2);
+
+    ststst(str1, str2, length1, length2);
     return 0;
 
 }
@@ -55,3 +59,43 @@ void string_scan(char **string){
 
 }
 
+int count_string(char * string){
+
+    int number = 0;
+    while(1){
+        if(string[number] == '\0'){
+            break;
+        }
+        number ++;
+    }
+    return number;
+
+}
+
+void ststst(char *str1, char *str2, int length1, int length2){
+
+    int count = 0;
+    int start = 0; 
+    for(int i = 0; i >= 0; i ++){
+        if(i > length1){ //若i超過str1的總字數則停止
+            printf("=> -1");
+            break;
+        }
+        //printf("i= %d, count數: %d\n", i, count);
+        if(str1[i] == str2[count]){ //判斷字元是否相同
+            if(count == 0){ //只記錄開始出現的位置
+                start = i;
+            }
+            count ++;
+            if(count == length2){ //若滿足str2的字數則判斷存在
+                printf("=> %d", start);
+                break;
+            }
+        }
+        else{ //若字元不相同則計數歸零
+            count = 0; 
+        }
+    }
+    printf("\n\n");
+
+}
