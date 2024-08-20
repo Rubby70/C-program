c34b3bf9f02dc3b515acbfcbe635031dff6abdcc
bobo <bear19990220@gmail.com>
2024-08-20 22:23:53 +0800

    0820

--- pratice/LinkedList3.c
+++ pratice/LinkedList3.c
@@ -20,15 +20,14 @@ typedef struct NODE node;
 void buildLL_2(node **ptr2LL_main, node *ptr, char carname[][10], int count){
 
     for(int i = 0; i < count; i ++){
-        strcpy(ptr->carlist.brandname, carname[i]);
+        strcpy(ptr[i].carlist.brandname, carname[i]);
         if(i == (count - 1)){
-            ptr[i].ptr2node = 0;
+            ptr[i].ptr2node = NULL;
         }
         else{
             ptr[i].ptr2node = &ptr[i + 1];
         }
-        strcpy(ptr->carlist.brandname, carname[i]);
-        printf("%d, %s\n", i, ptr->carlist.brandname);
+        printf("i: %d, ptr: %p, string: %s\n", i, &ptr[i], ptr[i].carlist.brandname);
     }
     *ptr2LL_main = &ptr[0];
 
@@ -41,21 +40,6 @@ int main(void){
     node *ptr2LL_main = NULL;
     buildLL_2(&ptr2LL_main, linkedlist, carname, 5);
 
-    //add
-    char target[5] = "C";
-    node additionallist;
-    int count = 0;
-    // while(ptr2LL_main != NULL){
-    //     if(ptr2LL_main->carlist.brandname == target){
-    //         printf("found: %d\n", count);
-    //         node *temp = ptr2LL_main->ptr2node;
-    //         ptr2LL_main->ptr2node = &additionallist;
-    //         additionallist.ptr2node = temp;
-    //         break;
-    //     }
-    //     ptr2LL_main = ptr2LL_main->ptr2node;
-    //     count ++;
-    // }
     while(ptr2LL_main != NULL){
         printf("%s\n", ptr2LL_main->carlist.brandname);
         ptr2LL_main = ptr2LL_main->ptr2node;
